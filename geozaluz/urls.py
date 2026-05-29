e1953873c3818e2d00d92747cda77c871bc33801
Cewield <orbitcs63@gmail.com>
2026-05-29 11:31:05 +0300

    urls.py

--- geozaluz/urls.py
+++ geozaluz/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Django is alive!")
+from geology.views import horizons_list, density_list
+from seismic_params.views import params_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', horizons_list, name='home'),
+    path('horizons/', horizons_list, name='horizons'),
+    path('params/', params_list, name='seismic_params_list'),
+    path('density/', density_list, name='density_list'),
 ]
