db3b4e0469c6f8f5b2b8395a87a5dd0a09a4f1dd
Cewield <orbitcs63@gmail.com>
2026-05-29 11:15:21 +0300

    urls.py

--- geozaluz/urls.py
+++ geozaluz/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from geology.views import horizons_list, density_list
 from seismic_params.views import params_list
-from frontpage.views import home   # додайте цей імпорт
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),                     # головна сторінка
+    path('', horizons_list, name='home'),           # головна сторінка – горизонти
     path('horizons/', horizons_list, name='horizons'),
     path('params/', params_list, name='seismic_params_list'),
     path('density/', density_list, name='density_list'),
