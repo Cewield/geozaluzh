ceef7b14f8dfb3ba182c3496e02382d3deeafa4d
Cewield <orbitcs63@gmail.com>
2026-05-29 11:19:55 +0300

    urls.py

--- geozaluz/urls.py
+++ geozaluz/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from geology.views import horizons_list, density_list
-from seismic_params.views import params_list
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Сайт працює!</h1><p>Якщо ви це бачите, Django налаштовано правильно.</p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', horizons_list, name='home'),           # головна сторінка – горизонти
-    path('horizons/', horizons_list, name='horizons'),
-    path('params/', params_list, name='seismic_params_list'),
-    path('density/', density_list, name='density_list'),
+    path('', home),
 ]
